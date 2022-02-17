e0eb7e0c2736bdb2d0a0a41bbe2a216de607282a
RCEman <theonekilaz@gmail.com>
2022-02-17 13:11:22 +0200

    CORS support added

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Misd\PhoneNumberBundle\MisdPhoneNumberBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
